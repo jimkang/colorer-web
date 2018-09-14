85bd879a8fd2cc1cb3154b03db71bb8ec90aeb5d
Jim Kang <jimkang@gmail.com>
2018-09-13 20:57:26 -0400

    Groundwork for supporting line filler.

--- app.js
+++ app.js
@@ -1,117 +1,27 @@
 var handleError = require('handle-error-web');
-var probable = require('probable');
 var qs = require('qs');
-var hsl = require('d3-color').hsl;
-
-var originalWidth;
-
-class ColorerApp {
-  constructor({
-    quant = 16,
-    grayscale = false,
-    srcImgUrl = 'data/fish.jpg',
-    recolorMode = 'random'
-  }) {
-    this.quantizationFactor = quant;
-    this.srcImgUrl = srcImgUrl;
-    this.grayscale = grayscale;
-    this.recolorMode = recolorMode;
-  }
-
-  loadSourceImage() {
-    var img = new Image();
-    img.crossOrigin = 'Anonymous';
-    img.addEventListener('load', doTransform.bind(this));
-    img.src = this.srcImgUrl;
-
-    function doTransform() {
-      const srcScale = 0.5;
-      this.scaleDownImage(img, srcScale);
-    }
-  }
-
-  scaleDownImage(img, scale) {
-    var srcCanvas = document.getElementById('source-canvas');
-    originalWidth = img.width;
-    var ctx = srcCanvas.getContext('2d');
-    var smallWidth = ~~(img.width * scale);
-    var smallHeight = ~~(img.height * scale);
-    srcCanvas.width = smallWidth;
-    srcCanvas.height = smallHeight;
-    if (this.grayscale) {
-      ctx.filter = 'saturate(0%)';
-    }
-    ctx.drawImage(img, 0, 0, smallWidth, smallHeight);
-    ctx.filter = 'none';
-    // Now we are in smallville.
-    var imageData = ctx.getImageData(0, 0, smallWidth, smallHeight);
-    this.recolor({
-      srcDataArray: Array.from(imageData.data),
-      smallWidth,
-      smallHeight,
-      scale: originalWidth / smallWidth
-    });
-  }
-
-  recolor({ srcDataArray, scale = 1.0, smallWidth, smallHeight }) {
-    var targetCanvas = document.getElementById('target-canvas');
-    targetCanvas.width = smallWidth * scale;
-    targetCanvas.height = smallHeight * scale;
-    var targetCtx = targetCanvas.getContext('2d');
-    var newForOld = {};
-
-    for (var i = 0; i < srcDataArray.length; i += 4) {
-      let rgbaArray = srcDataArray.slice(i, i + 4);
-      let originalString = this.rgbaToString(rgbaArray);
-      let replacement = newForOld[originalString];
-      if (!replacement) {
-        if (this.recolorMode === 'random') {
-          replacement = this.rgbaToString([
-            probable.roll(256),
-            probable.roll(256),
-            probable.roll(256),
-            255
-          ]);
-        } else if (this.recolorMode === 'shiftHue') {
-          let color = hsl(originalString);
-          color.h = probable.roll(360);
-          replacement = color.toString();
-        }
-        newForOld[originalString] = replacement;
-      }
-      // replacement = originalString;
-
-      let pixelIdx = i / 4;
-      let srcRow = ~~(pixelIdx / smallWidth);
-      let srcCol = pixelIdx % smallWidth;
-      let destX = srcCol * scale;
-      let destY = srcRow * scale;
-
-      targetCtx.fillStyle = replacement;
-      targetCtx.fillRect(destX, destY, scale, scale);
-    }
-
-    // targetCtx.fillStyle = 'blue';
-    // targetCtx.fillRect(0, 0, 1000, 1000);
-  }
-
-  rgbaToString(rgbaArray) {
-    return `rgba(${rgbaArray
-      .slice(0, 3)
-      .map(this.roundColorRawValue.bind(this))
-      .join(', ')}, ${rgbaArray[3] / 255})`;
-  }
-
-  roundColorRawValue(v) {
-    return ~~(v / this.quantizationFactor) * this.quantizationFactor;
-  }
-}
+var renderers = {
+  replacer: require('./replacer'),
+  linefiller: require('./linefiller')
+};
 
 (function go() {
   window.onerror = reportTopLevelError;
   const parms = qs.parse(window.location.hash.slice(1));
-  const app = new ColorerApp(parms);
-  app.loadSourceImage();
+  loadSourceImage();
+
+  function loadSourceImage() {
+    var img = new Image();
+    img.crossOrigin = 'Anonymous';
+    img.addEventListener('load', useImage);
+    img.src = parms.srcImgUrl || 'data/fish.jpg';
+  }
+
+  function useImage(e) {
+    var rendererOpts = Object.assign({ img: e.currentTarget }, parms);
+    var renderer = new renderers[parms.renderer || 'replacer'](rendererOpts);
+    renderer.start();
+  }
 })();
 
 function reportTopLevelError(msg, url, lineNo, columnNo, error) {
---
+++ replacer.js
@@ -0,0 +1,97 @@
+var probable = require('probable');
+var hsl = require('d3-color').hsl;
+
+var originalWidth;
+
+class Replacer {
+  constructor({ img, quant = 16, grayscale = false, recolorMode = 'random' }) {
+    this.quantizationFactor = quant;
+    this.grayscale = grayscale;
+    this.recolorMode = recolorMode;
+    this.img = img;
+  }
+
+  start() {
+    const srcScale = 0.5;
+    this.scaleDownImage(srcScale);
+  }
+
+  scaleDownImage(scale) {
+    var img = this.img;
+    var srcCanvas = document.getElementById('source-canvas');
+    originalWidth = img.width;
+    var ctx = srcCanvas.getContext('2d');
+    var smallWidth = ~~(img.width * scale);
+    var smallHeight = ~~(img.height * scale);
+    srcCanvas.width = smallWidth;
+    srcCanvas.height = smallHeight;
+    if (this.grayscale) {
+      ctx.filter = 'saturate(0%)';
+    }
+    ctx.drawImage(img, 0, 0, smallWidth, smallHeight);
+    ctx.filter = 'none';
+    // Now we are in smallville.
+    var imageData = ctx.getImageData(0, 0, smallWidth, smallHeight);
+    this.recolor({
+      srcDataArray: Array.from(imageData.data),
+      smallWidth,
+      smallHeight,
+      scale: originalWidth / smallWidth
+    });
+  }
+
+  recolor({ srcDataArray, scale = 1.0, smallWidth, smallHeight }) {
+    var targetCanvas = document.getElementById('target-canvas');
+    targetCanvas.width = smallWidth * scale;
+    targetCanvas.height = smallHeight * scale;
+    var targetCtx = targetCanvas.getContext('2d');
+    var newForOld = {};
+
+    for (var i = 0; i < srcDataArray.length; i += 4) {
+      let rgbaArray = srcDataArray.slice(i, i + 4);
+      let originalString = this.rgbaToString(rgbaArray);
+      let replacement = newForOld[originalString];
+      if (!replacement) {
+        if (this.recolorMode === 'random') {
+          replacement = this.rgbaToString([
+            probable.roll(256),
+            probable.roll(256),
+            probable.roll(256),
+            255
+          ]);
+        } else if (this.recolorMode === 'shiftHue') {
+          let color = hsl(originalString);
+          color.h = probable.roll(360);
+          replacement = color.toString();
+        }
+        newForOld[originalString] = replacement;
+      }
+      // replacement = originalString;
+
+      let pixelIdx = i / 4;
+      let srcRow = ~~(pixelIdx / smallWidth);
+      let srcCol = pixelIdx % smallWidth;
+      let destX = srcCol * scale;
+      let destY = srcRow * scale;
+
+      targetCtx.fillStyle = replacement;
+      targetCtx.fillRect(destX, destY, scale, scale);
+    }
+
+    // targetCtx.fillStyle = 'blue';
+    // targetCtx.fillRect(0, 0, 1000, 1000);
+  }
+
+  rgbaToString(rgbaArray) {
+    return `rgba(${rgbaArray
+      .slice(0, 3)
+      .map(this.roundColorRawValue.bind(this))
+      .join(', ')}, ${rgbaArray[3] / 255})`;
+  }
+
+  roundColorRawValue(v) {
+    return ~~(v / this.quantizationFactor) * this.quantizationFactor;
+  }
+}
+
+module.exports = Replacer;
