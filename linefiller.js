e95e30cdb91e63c2f9a186f1a6d48e65ad0d016c
adorsk <adorsk>
2018-10-04 20:59:41 -0400

    sanity check: redraw source image in target canvas

--- linefiller.js
+++ linefiller.js
@@ -4,95 +4,72 @@ var hsl = require('d3-color').hsl;
 var originalWidth;
 
 class LineFiller {
-  constructor({ img, quant = 16, grayscale = false, recolorMode = 'random' }) {
-    this.quantizationFactor = quant;
-    this.grayscale = grayscale;
-    this.recolorMode = recolorMode;
-    this.img = img;
+  constructor(routeOpts) {
+    this.quantizationFactor = routeOpts.quant;
+    this.grayscale = routeOpts.grayscale;
+    this.recolorMode = routeOpts.recolorMode;
+    this.img = routeOpts.img;
+    this.targetCanvas = routeOpts.targetCanvas;
+    this.showBase = routeOpts.showBase;
+    this.opacityPercentOverBase = routeOpts.opacityPercentOverBase;
+    this.numberOfRetriesToAvoidSingleColor = +routeOpts.numberOfRetriesToAvoidSingleColor;
+    this.minimumValueDifference = +routeOpts.minimumValueDifference;
   }
 
   start() {
-    const srcScale = 0.5;
-    console.log('Hey, line filler here. Remember to rewrite all of me!');
-    this.scaleDownImage(srcScale);
+    // Draw srcCanvas
+    const imgData = this.drawSrcCanvas();
+    this.recolor({imgData});
   }
 
-  scaleDownImage(scale) {
+  drawSrcCanvas () {
     var img = this.img;
     var srcCanvas = document.getElementById('source-canvas');
-    originalWidth = img.width;
     var ctx = srcCanvas.getContext('2d');
-    var smallWidth = ~~(img.width * scale);
-    var smallHeight = ~~(img.height * scale);
-    srcCanvas.width = smallWidth;
-    srcCanvas.height = smallHeight;
-    if (this.grayscale) {
-      ctx.filter = 'saturate(0%)';
-    }
-    ctx.drawImage(img, 0, 0, smallWidth, smallHeight);
-    ctx.filter = 'none';
-    // Now we are in smallville.
-    var imageData = ctx.getImageData(0, 0, smallWidth, smallHeight);
-    this.recolor({
-      srcDataArray: Array.from(imageData.data),
-      smallWidth,
-      smallHeight,
-      scale: originalWidth / smallWidth
-    });
+    srcCanvas.width = img.width
+    srcCanvas.height = img.height
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    // pass back imgData? or reference from this.bla
+    // for now: passing back.
+    return ctx.getImageData(0, 0, img.width, img.height);
   }
 
-  recolor({ srcDataArray, scale = 1.0, smallWidth, smallHeight }) {
-    var targetCanvas = document.getElementById('target-canvas');
-    targetCanvas.width = smallWidth * scale;
-    targetCanvas.height = smallHeight * scale;
-    var targetCtx = targetCanvas.getContext('2d');
-    var newForOld = {};
-
-    for (var i = 0; i < srcDataArray.length; i += 4) {
-      let rgbaArray = srcDataArray.slice(i, i + 4);
-      let originalString = this.rgbaToString(rgbaArray);
-      let replacement = newForOld[originalString];
-      if (!replacement) {
-        if (this.recolorMode === 'random') {
-          replacement = this.rgbaToString([
-            probable.roll(256),
-            probable.roll(256),
-            probable.roll(256),
-            255
-          ]);
-        } else if (this.recolorMode === 'shiftHue') {
-          let color = hsl(originalString);
-          color.h = probable.roll(360);
-          replacement = color.toString();
-        }
-        newForOld[originalString] = replacement;
+  recolor({imgData}) {
+    this.targetCanvas.width = imgData.width
+    this.targetCanvas.height = imgData.height
+    const targetCtx = this.targetCanvas.getContext('2d');
+    // Pick a direction: left-to-right
+    for (let y = 0; y < imgData.height; y++) {
+      // if (y == 10) { return }
+      // Watch out! be careful about edge cases, depends on direction.
+      // Literal edge cases :)
+      for (let x = 0; x < imgData.width - 1; x++) {
+        const srcRgba = this.getImgDataRgba({imgData, x, y});
+        targetCtx.strokeStyle = rgbaToString(srcRgba);
+        targetCtx.beginPath()
+        targetCtx.moveTo(x, y);
+        targetCtx.lineTo(x + 1, y);
+        targetCtx.stroke()
       }
-      // replacement = originalString;
-
-      let pixelIdx = i / 4;
-      let srcRow = ~~(pixelIdx / smallWidth);
-      let srcCol = pixelIdx % smallWidth;
-      let destX = srcCol * scale;
-      let destY = srcRow * scale;
-
-      targetCtx.fillStyle = replacement;
-      targetCtx.fillRect(destX, destY, scale, scale);
     }
+    // Expand a 'line': start w/ beginning color, go until we hit a new color, then
+    // fill in all the pixels up to the switch.
 
-    // targetCtx.fillStyle = 'blue';
-    // targetCtx.fillRect(0, 0, 1000, 1000);
+    // For extra credit: weave perpendicular directions! (alternate pixels?)
   }
 
-  rgbaToString(rgbaArray) {
-    return `rgba(${rgbaArray
-      .slice(0, 3)
-      .map(this.roundColorRawValue.bind(this))
-      .join(', ')}, ${rgbaArray[3] / 255})`;
+  getImgDataRgba({imgData, x, y}) {
+    const startIdx = ((imgData.width * y) + x) * 4
+    const rgba = [0, 1, 2, 3].map(function fishPie (i) {
+      return imgData.data[startIdx + i]
+    })
+    return rgba
   }
 
-  roundColorRawValue(v) {
-    return ~~(v / this.quantizationFactor) * this.quantizationFactor;
-  }
+}
+
+function rgbaToString(rgbaArray) {
+  return `rgba(${rgbaArray.slice(0, 3).join(', ')}, ${rgbaArray[3] / 255})`;
 }
 
 module.exports = LineFiller;
