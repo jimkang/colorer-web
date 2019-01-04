a6db4f85eee64c2757bcf96ab55ca4378997b9f9
Jim Kang <jimkang@gmail.com>
2019-01-03 21:20:12 -0500

    Successfully copy from one canvas to another! (This is actually a good boilerplate for future renderers.

--- igniter.js
+++ igniter.js
@@ -1,11 +1,11 @@
-var getReplacementColor = require('./getReplacementColor');
-
 class Igniter {
   constructor(routeOpts) {
     this.img = routeOpts.img;
     this.targetCanvas = routeOpts.targetCanvas;
+    this.targetCtx = this.targetCanvas.getContext('2d');
     this.showBase = routeOpts.showBase;
     this.opacityPercentOverBase = routeOpts.opacityPercentOverBase;
+    this.boundDrawFrame = this.drawFrame.bind(this);
   }
 
   start() {
@@ -14,10 +14,10 @@ class Igniter {
     // Init stuff before animation.
     this.currentSrcRow = 0; // names :)
     // do request animation frame here.
-    requestAnimationFrame(() => {
-      this.drawFrame()
-      // draw frame!
-    })
+    for (var i = 0; i < this.imgData.height; ++i) {
+      this.drawFrame();
+    }
+    //requestAnimationFrame(this.boundDrawFrame);
   }
 
   initCanvases() {
@@ -26,19 +26,39 @@ class Igniter {
     var ctx = srcCanvas.getContext('2d');
     srcCanvas.width = img.width;
     srcCanvas.height = img.height;
+    this.outputBuffer = new Uint8ClampedArray(img.width * img.height * 4);
     ctx.drawImage(img, 0, 0, img.width, img.height);
-    this.targetCanvas.width = imgData.width;
-    this.targetCanvas.height = imgData.height;
+    this.targetCanvas.width = img.width;
+    this.targetCanvas.height = img.height;
     // pass back imgData? or reference from this.bla
     // for now: passing back.
     return ctx.getImageData(0, 0, img.width, img.height);
   }
 
-  drawFrame () {
-    // get current line of srcImage.
-    for (let x = 0; x < imgData.width; x++) {
+  drawFrame() {
+    if (this.currentSrcRow >= this.imgData.height) {
+      console.log(this.currentSrcRow, this.imgData.height);
+      return;
     }
-    // bump row.
+
+    // get current line of srcImage.
+    for (let x = 0; x < this.imgData.width; x++) {
+      const pixelIndex = (this.imgData.width * this.currentSrcRow + x) * 4;
+      for (let rgbaIndex = 0; rgbaIndex < 4; ++rgbaIndex) {
+        const i = pixelIndex + rgbaIndex;
+        this.outputBuffer[i] = this.imgData.data[i];
+      }
+    }
+
+    this.targetCtx.putImageData(
+      new ImageData(this.outputBuffer, this.imgData.width, this.imgData.height),
+      0,
+      0
+    );
+    this.currentSrcRow += 1;
+
+    //requestAnimationFrame(this.boundDrawFrame);
+    //setTimeout(this.boundDrawFrame, 0);
   }
 }
 
